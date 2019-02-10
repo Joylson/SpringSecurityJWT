91972ad845632f7937278264860e45053d1a11e1
j.miranda <joylsont@gmail.com>
2019-02-10 15:09:55 -0200

    Publicado Estrutura do projeto

---
+++ SecurityJWT/src/test/java/br/com/securityjwt/SecurityJwtApplicationTests.java
@@ -0,0 +1,17 @@
+package br.com.securityjwt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SecurityJwtApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
